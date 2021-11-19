a210a237b7e537593d19ebe96e8d978d3fa0efa6
rocunha09 <ro.cunha000@gmail.com>
2021-11-19 03:42:53 -0300

    realizando busca por uma frase específica atráves do seu id com ajax e método get

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,6 @@
 $("#botao-frase").click(fraseAleatoria)
+$("#botao-frase-id").click(buscarFrase)
+
 
 //realizando ajax com jquery
 //realiza chamada get em uma url e recebe na função de callback os dados retornados (data)
@@ -9,7 +11,25 @@ function fraseAleatoria(){
        
     setTimeout(() => {
             $("#erro").toggle()
-        }, 2000);    
+        }, 2000)
+
+    }).always(()=>{
+        $("#spinner").toggle()
+    })
+}
+
+function buscarFrase(){
+    var fraseId = $("#frase-id").val() == "" ? "0" : $("#frase-id").val() 
+    dados = {id: fraseId}
+
+    $("#spinner").show()
+    $.get("http://localhost:3000/frases", dados, trocaFraseUnica).fail(()=>{
+        $("#erro").show()
+        
+        setTimeout(() => {
+            $("#erro").toggle()
+        }, 2000)
+
     }).always(()=>{
         $("#spinner").toggle()
     })
@@ -22,4 +42,13 @@ function trocaFrase(data){
 
     atualizaTamanhoFrase()
     atualizaTempoInicial(data[numFrase].tempo)
+}
+
+
+function trocaFraseUnica(data){
+    var frase = $(".frase")
+    frase.text(data.texto)
+
+    atualizaTamanhoFrase()
+    atualizaTempoInicial(data.tempo)
 }
